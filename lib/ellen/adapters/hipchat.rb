893387ca917182341feb0ccc70b6752a5041233a
Ryo Nakamura <r7kamura@gmail.com>
2014-04-12 22:58:01 +0900

    Add empty Ellen::Adapters::Hipchat class

---
+++ lib/ellen/adapters/hipchat.rb
@@ -0,0 +1,10 @@
+module Ellen
+  module Adapters
+    class Hipchat < Base
+      # TODO
+      def run
+        puts "#{self.class}##{__method__} is not yet implemented"
+      end
+    end
+  end
+end
--- lib/ellen/hipchat.rb
+++ lib/ellen/hipchat.rb
@@ -1,7 +1,2 @@
+require "ellen/adapters/hipchat"
 require "ellen/hipchat/version"
-
-module Ellen
-  module Hipchat
-    # Your code goes here...
-  end
-end
